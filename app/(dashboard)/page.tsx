b0da92a64c63851ce06a764b2acd4a4deb5a73dd
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-06-04 22:47:54 +0700

    first commit

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <main className="flex flex-col items-center justify-center gap-y-4">
+      <div>Dashboard Root Page</div>
+    </main>
+  );
+}
---
+++ components/auth/loading.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Loading() {
+  return (
+    <div className="h-full w-full flex flex-col gap-y-4 justify-center items-center">
+      <Image
+        src="/logo.svg"
+        width={120}
+        height={120}
+        alt="logo"
+        className="animate-pulse duration-700"
+      />
+    </div>
+  );
+}
